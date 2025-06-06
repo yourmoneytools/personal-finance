353e89ee6cdcd2ec478edfb0833f46ff5bf0cacb
Robert Garcia <robertserranogar@gmail.com>
2025-06-05 20:24:46 -0600

    enw

--- external-scripts.js
+++ external-scripts.js
@@ -1,1 +1,11 @@
 // add all your scripts for analytics and tracking here
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HGW5X87VBP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HGW5X87VBP');
+</script>
