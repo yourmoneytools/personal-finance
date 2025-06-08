8f250498450e7b558509deadf1612af830da72ba
Robert Garcia <robertserranogar@gmail.com>
2025-06-08 12:21:21 -0600

    new

--- external-scripts.js
+++ external-scripts.js
@@ -2,7 +2,18 @@
 
 // google analytics
 window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-		gtag('config', 'G-HGW5X87VBP')
+gtag('config', 'G-HGW5X87VBP')
+
+
+// Hotjar Tracking Code for https://www.cashtools.org/ 
+(function(h,o,t,j,a,r){
+h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+h._hjSettings={hjid:6429030,hjsv:6};
+a=o.getElementsByTagName('head')[0];
+r=o.createElement('script');r.async=1;
+r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
